72d6267787eda1a87fdc35a5dcbebf6f667e2565
xl1 <shiba200712@gmail.com>
2015-11-08 22:48:49 +0900

    Suppress null reference errors

--- resource/content_script.js
+++ resource/content_script.js
@@ -1,7 +1,6 @@
 (function() {
-  var first = document.body.firstElementChild;
-  if(!first) return;
-  if(first.textContent === document.body.textContent){
+  var first = document && document.body && document.body.firstElementChild;
+  if(first && first.textContent === document.body.textContent){
     chrome.runtime.sendMessage('showPageAction');
     if(/\.(markdown|md)$/.test(document.URL)){
       chrome.runtime.sendMessage('markdownize');
