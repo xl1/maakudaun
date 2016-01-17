1cf6c5d1e2af645fec1dd05f8d32d342103b7a01
xl1 <shiba200712@gmail.com>
2016-01-17 15:39:04 +0900

    Detect .md/.markdown extension properly

--- resource/content_script.js
+++ resource/content_script.js
@@ -2,7 +2,7 @@
   var first = document && document.body && document.body.firstElementChild;
   if(first && first.textContent === document.body.textContent){
     chrome.runtime.sendMessage('showPageAction');
-    if(/\.(markdown|md)$/.test(document.URL)){
+    if(/\.(markdown|md)$/.test(location.pathname)){
       chrome.runtime.sendMessage('markdownize');
     }
   }
